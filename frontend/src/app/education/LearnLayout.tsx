45ceec0732077552ebf1f73c2b8b026e10a35d7a
Federico Tafur <fedetafur@vt.edu>
2026-04-02 14:29:30 -0500

    fix: Use standard SiteHeader on Learn page for consistent layout

--- frontend/src/app/education/LearnLayout.tsx
+++ frontend/src/app/education/LearnLayout.tsx
@@ -1,6 +1,6 @@
 import { Outlet } from 'react-router-dom';
 import { AppSidebar } from '@/components/app-sidebar';
-import LearnHeader from '@/components/layout/LearnHeader';
+import { SiteHeader } from '@/components/site-header';
 import { SidebarInset, SidebarProvider } from '@/components/ui/sidebar';
 import { useEffect } from 'react';
 import { useEducationStore } from '@/store/educationStore';
@@ -47,9 +47,11 @@ export default function LearnLayout() {
         } as React.CSSProperties}
       >
         <div className="flex flex-1 flex-col">
-          <LearnHeader />
-          <div className="@container/main flex flex-1 flex-col">
-            <Outlet />
+          <SiteHeader />
+          <div className="@container/main flex flex-1 flex-col gap-2">
+            <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6 px-4 lg:px-6">
+              <Outlet />
+            </div>
           </div>
         </div>
       </SidebarInset>
