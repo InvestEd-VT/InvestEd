18f5ea6fca86dfa6e020c6bfd093b3a3ff2cbbf0
lucasvt22 <lucas22@vt.edu>
2026-04-07 10:10:30 -0400

    feat: create options exercise service, handle ITM auto exercise, handle OTM expiration [INVESTED-255, INVESTED-256, INVESTED-257]

--- backend/src/services/index.ts
+++ backend/src/services/index.ts
@@ -4,3 +4,4 @@ export * from './user.service.js';
 export * from './email.service.js';
 export * from './portfolio.service.js';
 export * from './education.service.js';
+export * from './exercise.service.js';
